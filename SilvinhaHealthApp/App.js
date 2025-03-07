df44a8b472569538683cf3125aa8f54b5a68c239
LuksBre <lucasbregadioli8@gmail.com>
2025-03-07 12:28:11 -0300

    incluindo inputs

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Silvinha Health App</Text>
-        <Text style={style.textle}> silvinha tem 800 anos e revela seu segredo </Text>
+        
      </View>
-    </View>
+     <View style={styles.content}>
+        <Text style={styles.subTitle}>Calculadora de IMC</Text>
+     </View>
+        
+      <View>
+        <Text Style={styles.label}>Altura</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Ex. 1.70'
+          keyboardType='numeric'>
+        </TextInput>
+      </View>
+     <StatusBar style='light'>
+
+     </StatusBar>
+    </SafeAreaView>
   );
 }
 
@@ -16,21 +31,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e0e5e5',
-    paddingTop: 50,
+
   },
   titleContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
+    justifyContent: 'flex-end',
+    height: 130,
+    backgroundColor: '#D90429',
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25
   },
   title: {
-    color: "#ff0043",
+    color: "#EDF2F4",
     fontSize: 28,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 30,
+  }, 
+  content: {
+    flex: 1,
+    padding: 40,
+    width: '100%',
+    backgroundColor: '#EDF2F4'
   },
-  textle:{
-    color: 'blue'
+  subTitle: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: "#D90429",
+    fontWeight: 'bold',
+    marginBottom: 40,
     
+  },
+  label:{
+    color: '#000',
+    fontSize: 18,
+  },
+  input:{
+    height: 45,
+    width: '100%',
+    fontSize: 18,
+    borderColor: '#D90429',
+    borderBottomWidth: 1
   }
-
 });
